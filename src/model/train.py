61d25567b2484ecdc19db26be6ce35e1a921f857
Espoir Badohoun <espoir.badohoun@gmail.com>
2024-01-19 15:23:57 +0100

    My datascientist refactoring

--- src/model/train.py
+++ src/model/train.py
@@ -1,74 +1,90 @@
-# Import libraries
-
-import argparse
-import glob
-import os
-
+from pathlib import Path
+from sklearn.linear_model import Ridge
+from sklearn.base import RegressorMixin
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import mlflow
+import mlflow.sklearn
+from typing import Tuple , Dict
+from typing_extensions import Annotated
 import pandas as pd
+import joblib 
 
-from sklearn.linear_model import LogisticRegression
-
-
-# define functions
-def main(args):
-    # TO DO: enable autologging
+THIS_DIR = Path(__file__).parent
+DIABETES_DEV_CSVPATH = (THIS_DIR  / "/Users/espoirbadohoun/RefactorDatascientistCode/experimentation/data/diabetes-dev.csv").resolve()
+print(DIABETES_DEV_CSVPATH)
 
 
-    # read data
-    df = get_csvs_df(args.training_data)
-
-    # split data
-    X_train, X_test, y_train, y_test = split_data(df)
-
-    # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
-def get_csvs_df(path):
-    if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = glob.glob(f"{path}/*.csv")
-    if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+def load_data(path:str) -> pd.DataFrame :
+     df = pd.read_csv(path)
+     return df 
 
 
-# TO DO: add function to split data
+
+# Split the dataframe into test and train data
+def split_data(df:pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+]:
+
+      
+    X = df.drop('Diabetic', axis=1).values
+    y = df['Diabetic'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0)
+    
+    return X_train, X_test, y_train, y_test
 
 
-def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+# Train the model, return the model
+def train_model(X_train:pd.DataFrame, y_train:pd.Series ,  args: Dict[str, float]) -> RegressorMixin:
+    reg_model = Ridge(**args)
+    reg_model.fit(X_train,  y_train)
+    return reg_model
 
 
-def parse_args():
-    # setup arg parser
-    parser = argparse.ArgumentParser()
+# Evaluate the metrics for the model
+def get_model_metrics(reg_model:RegressorMixin, X_test:pd.DataFrame , y_test:pd.Series) -> Annotated[float, "r2_score"] :
+    preds = reg_model.predict(X_test)
+    mse = mean_squared_error(preds, y_test)
+    metrics = {"mse": mse}
+    return metrics
 
-    # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
 
-    # parse args
-    args = parser.parse_args()
+def main():
+    # Start MLflow run
+    with mlflow.start_run():
 
-    # return args
-    return args
+        # Load Data
+        sample_data = load_data(DIABETES_DEV_CSVPATH)
 
-# run script
-if __name__ == "__main__":
-    # add space in logs
-    print("\n\n")
-    print("*" * 60)
+        # Split Data into Training and Validation Sets
+        X_train, X_test, y_train, y_test  = split_data(sample_data)
 
-    # parse args
-    args = parse_args()
+        # Train Model on Training Set
+        args = {
+            "alpha": 0.5
+        }
+        reg = train_model(X_train, y_train, args)
 
-    # run main function
-    main(args)
+        # Validate Model on Validation Set
+        metrics = get_model_metrics(reg, X_test , y_test)
 
-    # add space in logs
-    print("*" * 60)
-    print("\n\n")
+        # Log parameters and metrics
+        mlflow.log_params(args)
+        mlflow.log_metrics(metrics)
+
+        # Save Model
+        model_name = "sklearn_regression_model.pkl"
+        joblib.dump(value=reg, filename=model_name)
+
+        # Log model artifact
+        mlflow.sklearn.log_model(reg, "model")
+
+if __name__ == '__main__':
+    main()
